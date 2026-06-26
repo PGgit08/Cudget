dbe47f78ba11ac2211aa3f85baac58ce86da938e
Peter Gutkovich <peter.mty8@gmail.com>
2026-06-25 22:02:03 -0400

    Initial Commit

---
+++ Cudget/CudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CudgetApp.swift
+//  Cudget
+//
+//  Created by Peter Gutkovich on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct CudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
